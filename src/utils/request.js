c788b409311788eab8363300cfb4b35ff1ff1ca4
Ruqian <3169834730@qq.com>
2026-05-19 13:08:39 +0800

    axios二次封装

---
+++ src/utils/request.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { ElMessage } from "element-plus";
+
+//创建axios实例
+const service = axios.create({
+  baseURL: "/api", //请求前缀，所有请求都会自动加上/api
+  timeout: 5000, //请求超时时间
+});
+
+//请求拦截器
+service.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token"); //从localStorage中获取token
+    if (token) {
+      config.headers.Authorization = token; //将token添加到请求头中
+    }
+    return config; //后端判断用户是否登录并返回配置对象
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+//响应拦截器
+service.interceptors.response.use(
+  (response) => {
+    const { data, config } = response;
+    //处理业务状态码
+    if (data.code === 200) {
+      return data.data; //返回业务数据
+    } else {
+      if (data.code === "-1") {
+        //若登录过期
+        if (!config.url.includes("/login")) {
+          ElMessage.error(data.msg || "登录过期，请重新登录");
+          //清除登录数据
+          localStorage.removeItem("token");
+          localStorage.removeItem("userInfo");
+          window.location.href = "/auth/login";
+        } else {
+          ElMessage.error(data.msg || "登录过期，请重新登录");
+          return Promise.reject("网络请求失败....");
+        }
+      }
+    }
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default service;
